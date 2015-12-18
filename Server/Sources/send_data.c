847971c1936dc1eb32af2f069cc04e40aaa87213
dany <dany.bouca-nova@epitech.eu>
2015-12-18 18:06:22 +0100

    Project

---
+++ Server/Sources/send_data.c
@@ -0,0 +1,45 @@
+/*
+** send_data.c for zappy in /home/bouca-_d/rendu/Zappy/Server
+**
+** Made by Dany Bouca Nova
+** Login   <bouca-_d@epitech.net>
+**
+** Started on  Sun Jul  5 02:04:21 2015 Dany Bouca Nova
+** Last update Sun Jul  5 16:15:30 2015 Dany Bouca Nova
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "server.h"
+
+extern t_serv	g_serv;
+
+void		send_to_client(t_player* player)
+{
+  char		*ptr;
+
+  if (player != NULL)
+    {
+      ptr = pop_first(&player->send);
+      if (ptr != NULL)
+	{
+	  dprintf(player->fd, "%s\n", ptr);
+	  free(ptr);
+	}
+    }
+}
+
+void		send_to_graph(t_graph* graph)
+{
+  char		*ptr;
+
+  if (graph)
+    {
+      ptr = pop_first(&graph->send);
+      if (ptr != NULL)
+	{
+	  dprintf(graph->fd, "%s\n", ptr);
+	  free(ptr);
+	}
+    }
+}
